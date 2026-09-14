ab7365fc171cc2b186553cbc52f428e0d30f5d53
HienPahm <doangiahienpham@gmail.com>
2026-09-15 02:10:53 +0800

    Give empty-list, no-match, and unmark messages more personality

--- src/main/java/will/command/ListCommand.java
+++ src/main/java/will/command/ListCommand.java
@@ -8,6 +8,10 @@ import will.Ui;
 public class ListCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
+        if (tasks.size() == 0) {
+            ui.showMessage("Your list's empty! Nothing on your plate right now.");
+            return;
+        }
         ui.showMessage("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             ui.showMessage((i + 1) + "." + tasks.get(i).toString());
